39ca9422c447c14c957ea872031235291793b7a9
Junior <juniorantoniocalvomontanez@gmail.com>
2021-01-17 01:29:50 -0600

    using python in visual studio code

---
+++ DailyRainfall.py
@@ -0,0 +1,15 @@
+# we have to install netcdf4 in Anaconda Prompt (anaconda 3)
+#then you write "conda intsall netcdf4"
+#write in Proceed "y" to downloading and extracting packages
+# use "python -m pip show netcdf4" and "conda list netcdf4" to see the version 
+ import requests
+ import netcdf4 as nc
+ import numpy as np
+
+2+3
+
+
+
+
+
+ 
